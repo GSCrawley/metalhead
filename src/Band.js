b284038cacb4194efdd57558b78c94ff7ad3fd3a
GSCrawley <gscrawley@gmail.com>
2020-03-05 15:48:22 -0800

    made like buttons

--- src/Band.js
+++ src/Band.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './band.css'
+import Like from './Like'
 
 class Band extends Component {
 
@@ -11,6 +12,7 @@ class Band extends Component {
           <h3>Formed: {this.props.formed}</h3>
           <h3>origin: {this.props.origin}</h3>
           <h3>fans: {this.props.fans}</h3>
+        <Like />
         </div>
       )
     }

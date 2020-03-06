2ff6a3401a435f77639d106d75a9df2d5aaa74e0
GSCrawley <gscrawley@gmail.com>
2020-03-06 09:20:15 -0800

    Updates

---
+++ precache-manifest.450cc33ecdef0c5386f23faeae6b63af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485c07b2197f530d6e65a94e99898740",
+    "url": "/metalhead/index.html"
+  },
+  {
+    "revision": "acd51fc2697d3c79409f",
+    "url": "/metalhead/static/css/main.0c074b47.chunk.css"
+  },
+  {
+    "revision": "98ac7e4391f3fc5e805b",
+    "url": "/metalhead/static/js/2.351c5111.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/metalhead/static/js/2.351c5111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd51fc2697d3c79409f",
+    "url": "/metalhead/static/js/main.22e6a76c.chunk.js"
+  },
+  {
+    "revision": "46475a718f9e3e8f738a",
+    "url": "/metalhead/static/js/runtime-main.321ee083.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/metalhead/static/media/logo.5d5d9eef.svg"
+  }
+]);
